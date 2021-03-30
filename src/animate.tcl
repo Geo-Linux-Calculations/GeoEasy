f9b7203aa204bffeef267ffdbb219b47b79ff1e8
zvezdochiot <mykaralw@yandex.ru>
2021-03-30 18:45:50 +0300

    3.1.4: arc length added to arc setting out output

--- src/animate.tcl
+++ src/animate.tcl
@@ -6,7 +6,7 @@ proc init_animate {rw} {
     global animate geoEasyMsg
 	global tcl_platform
 	global home
-    global f1_icon f2_icon about_icon zoom_in_icon zoom_out_icon zoom_prev_icon pan_icon area_icon newp_icon ruler_icon sp1_icon reg_icon zdtm_icon breakline_icon hole_icon xchgtri_icon
+    global f1_icon f2_icon
     global globe_icon
     global icon_status
 
